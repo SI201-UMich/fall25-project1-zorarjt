5e8831a04fb70655539acff3a6f7792698ae4187
Zora Tucker <zrtucker@umich.edu>
2025-10-01 21:29:38 -0400

    added name header and finished part 2

--- Project1.py
+++ Project1.py
@@ -1,3 +1,9 @@
+# Name: Zora Tucker
+# Student ID: 28071696
+# Email: zrtucker@umich.edu
+# Worked Alone
+# GenAI Use:
+
 import csv
 
 # Part 2
@@ -8,3 +14,22 @@ def read_crop_yield_headers():
         header = next(csv_read)
         return header
 print(read_crop_yield_headers())
+
+# sample entry (row) in the dataset
+def read_crop_yield_entry():
+    with open("/Users/zoratucker/Downloads/SI 201/fall25-project1-zorarjt/crop_yield.csv") as csv_file:
+        csv_read=csv.reader(csv_file, delimiter=',')
+        next(csv_read)
+        entry = next(csv_read)
+        return entry
+print(read_crop_yield_entry())
+
+# number of rows in the dataset
+def read_crop_yield_num_rows():
+    with open("/Users/zoratucker/Downloads/SI 201/fall25-project1-zorarjt/crop_yield.csv") as csv_file:
+        csv_read=csv.reader(csv_file, delimiter=',')
+        next(csv_read)
+        for num_rows, row in enumerate(csv_read):
+            num_rows += 1 
+        return num_rows
+print(read_crop_yield_num_rows())
